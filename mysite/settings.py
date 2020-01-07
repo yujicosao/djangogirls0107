aa77c7820e9468b89724cb0d4ab277212bd5eeeb
小棹雄司 <kosaoyuuji@kosaoyuujinoMacBook-Pro.local>
2020-01-07 14:51:30 +0900

    initial commit4

--- mysite/settings.py
+++ mysite/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True  # Trueから変更
+DEBUG = False  # Trueから変更
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
