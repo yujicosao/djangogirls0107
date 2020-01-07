cfb0a8243a6551db054eb44e0f32f38d9f88222c
小棹雄司 <kosaoyuuji@kosaoyuujinoMacBook-Pro.local>
2020-01-07 15:36:48 +0900

    initial commit5

--- blog/views.py
+++ blog/views.py
@@ -33,7 +33,12 @@ def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
         form = PostForm(request.POST, instance=post)
-
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
-    return render(request, 'blog/post_new.html', {'form': form})
+    return render(request, 'blog/post_edit.html', {'form': form})
